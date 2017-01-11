26afe06a674ccd233e21658b732142c76f050ed6
Kaeltis <info@kaeltis.de>
2017-01-11 21:33:27 +0100

    trying to fix lag for region select

--- map.js
+++ map.js
@@ -13,6 +13,33 @@ var map = new mapboxgl.Map({
 
 map.getCanvas().style.cursor = 'default';
 
+// define layer names
+var namings = {
+    "bahn-75": "Bahn - DB",
+    "bahn-70": "Bahn - DB",
+    "bahn-65": "Bahn - DB",
+    "bahn-60": "Bahn - DB",
+    "bahn-55": "Bahn - DB",
+    "bahn-kvb-hgk-75": "Bahn - KVB & HGK",
+    "bahn-kvb-hgk-70": "Bahn - KVB & HGK",
+    "bahn-kvb-hgk-65": "Bahn - KVB & HGK",
+    "bahn-kvb-hgk-60": "Bahn - KVB & HGK",
+    "bahn-kvb-hgk-55": "Bahn - KVB & HGK",
+    "industrie-hafen-75": "Industrie & Häfen",
+    "industrie-hafen-70": "Industrie & Häfen",
+    "industrie-hafen-65": "Industrie & Häfen",
+    "industrie-hafen-60": "Industrie & Häfen",
+    "industrie-hafen-55": "Industrie & Häfen",
+    "strasse-75": "Straße",
+    "strasse-70": "Straße",
+    "strasse-65": "Straße",
+    "strasse-60": "Straße",
+    "strasse-55": "Straße",
+    "gruen": "Grünfläche"
+};
+var layers = Object.keys(namings);
+var legendLayers = ['bahn-75', 'bahn-kvb-hgk-75', 'industrie-hafen-75', 'strasse-75', 'gruen'];
+
 var ctx = document.getElementById("piechart");
 var pieChart = new Chart(ctx, {
     type: 'pie',
@@ -47,33 +74,25 @@ var pieChart = new Chart(ctx, {
 
 // wait for map to load before adjusting it
 map.on('load', function () {
-
-    // define layer names
-    var namings = {
-        "bahn-75": "Bahn - DB",
-        "bahn-70": "Bahn - DB",
-        "bahn-65": "Bahn - DB",
-        "bahn-60": "Bahn - DB",
-        "bahn-55": "Bahn - DB",
-        "bahn-kvb-hgk-75": "Bahn - KVB & HGK",
-        "bahn-kvb-hgk-70": "Bahn - KVB & HGK",
-        "bahn-kvb-hgk-65": "Bahn - KVB & HGK",
-        "bahn-kvb-hgk-60": "Bahn - KVB & HGK",
-        "bahn-kvb-hgk-55": "Bahn - KVB & HGK",
-        "industrie-hafen-75": "Industrie & Häfen",
-        "industrie-hafen-70": "Industrie & Häfen",
-        "industrie-hafen-65": "Industrie & Häfen",
-        "industrie-hafen-60": "Industrie & Häfen",
-        "industrie-hafen-55": "Industrie & Häfen",
-        "strasse-75": "Straße",
-        "strasse-70": "Straße",
-        "strasse-65": "Straße",
-        "strasse-60": "Straße",
-        "strasse-55": "Straße",
-        "gruen": "Grünfläche"
-    };
-    var layers = Object.keys(namings);
-    var legendLayers = ['bahn-75', 'bahn-kvb-hgk-75', 'industrie-hafen-75', 'strasse-75', 'gruen'];
+    map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'paint': {
+            'fill-extrusion-color': '#aaa',
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'height'
+            },
+            'fill-extrusion-base': {
+                'type': 'identity',
+                'property': 'min_height'
+            },
+            'fill-extrusion-opacity': .6
+        }
+    });
 
     // create legend
     legendLayers.forEach(function (layer) {
@@ -96,7 +115,7 @@ map.on('load', function () {
             layers: layers
         });
 
-        var hasgreen = false;
+        var hasGreen = false;
 
         if (regions.length > 0) {
             document.getElementById('pd').innerHTML = "";
@@ -104,7 +123,11 @@ map.on('load', function () {
                 if (region.layer.id == 'gruen') {
                     // Grünfläche
                     document.getElementById('pd').innerHTML += "<h3><strong>" + namings[region.layer.id] + "</strong></h3><p><em>" + region.properties.Name + "</em></p><p><em>Größe: <strong>" + parseFloat(turf.area(region) / 10000).toFixed(2) + " Hektar</strong></em></p>";
-                    hasgreen = true;
+                    if (!hasGreen) {
+                        hasGreen = true;
+                        currentGreenRegion = region;
+                        currentPoint = e.point;
+                    }
                 } else {
                     // Keine Grünfläche
                     document.getElementById('pd').innerHTML += "<h3><strong>" + namings[region.layer.id] + "</strong></h3><p><em>" + region.properties.TEXT + "</em></p>";
@@ -114,19 +137,25 @@ map.on('load', function () {
             document.getElementById('pd').innerHTML = '<p>Please hover over a region!</p>';
         }
 
-        if (hasgreen) {
+        if (hasGreen) {
             document.getElementById('piechart').style.visibility = "visible";
-            updateChartValues(regions);
-            pieChart.update();
         } else {
             document.getElementById('piechart').style.visibility = "hidden";
+            currentGreenRegion = undefined;
+            currentPoint = undefined;
         }
     });
 
 });
 
-function updateChartValues(regions) {
-    var gruenregion;
+var currentGreenRegion;
+var currentPoint;
+updateChart();
+function updateChart() {
+    if (!(currentGreenRegion != undefined && currentPoint != undefined)) {
+        setTimeout(updateChart, 100);
+        return;
+    }
 
     pieChart.data.datasets[0].data[0] = 0;
     pieChart.data.datasets[0].data[1] = 0;
@@ -134,30 +163,47 @@ function updateChartValues(regions) {
     pieChart.data.datasets[0].data[3] = 0;
     pieChart.data.datasets[0].data[4] = 0;
 
-    regions.forEach(function (region) {
-        if (region.layer.id == 'gruen') {
-            gruenregion = region;
+    var clone = layers.slice(0);
+    clone.pop();
+
+    var greenSize = turf.area(currentGreenRegion);
+    var onePercentGreen = greenSize / 100;
+
+    /*
+     var regionsToCheck = map.queryRenderedFeatures({
+     layers: clone
+     });
+     */
+
+    //TODO: better calculate size to check for
+    //too big = lag, too small = missing layers
+    var width = map.getZoom() * 15;
+    var height = map.getZoom() * 15;
+    var regionsToCheck = map.queryRenderedFeatures([
+        [currentPoint.x - width / 2, currentPoint.y - height / 2],
+        [currentPoint.x + width / 2, currentPoint.y + height / 2]
+    ], {
+        layers: clone
+    });
+
+    regionsToCheck.forEach(function (region) {
+        var intersection = turf.intersect(currentGreenRegion, region);
+
+        if (intersection != undefined) {
+            if (region.layer.id.includes('bahn-kvb-hgk'))
+                pieChart.data.datasets[0].data[1] += turf.area(intersection) / onePercentGreen;
+            else if (region.layer.id.includes('bahn'))
+                pieChart.data.datasets[0].data[0] += turf.area(intersection) / onePercentGreen;
+            else if (region.layer.id.includes('industrie-hafen'))
+                pieChart.data.datasets[0].data[2] += turf.area(intersection) / onePercentGreen;
+            else if (region.layer.id.includes('strasse'))
+                pieChart.data.datasets[0].data[3] += turf.area(intersection) / onePercentGreen;
         }
     });
 
-    if (gruenregion != undefined && regions.length > 1) {
-        var gruenarea = turf.area(gruenregion);
-
-        var onepercent = gruenarea / 100;
-
-        regions.forEach(function (region) {
-            if (region.layer.id != 'gruen') {
-                if (region.layer.id.includes('bahn-kvb-hgk'))
-                    pieChart.data.datasets[0].data[1] = turf.area(turf.intersect(gruenregion, region)) / onepercent;
-                else if (region.layer.id.includes('bahn'))
-                    pieChart.data.datasets[0].data[0] = turf.area(turf.intersect(gruenregion, region)) / onepercent;
-                else if (region.layer.id.includes('industrie-hafen'))
-                    pieChart.data.datasets[0].data[2] = turf.area(turf.intersect(gruenregion, region)) / onepercent;
-                else if (region.layer.id.includes('strasse'))
-                    pieChart.data.datasets[0].data[3] = turf.area(turf.intersect(gruenregion, region)) / onepercent;
-            }
-        });
-    }
-
     pieChart.data.datasets[0].data[4] = 100 - pieChart.data.datasets[0].data[3] - pieChart.data.datasets[0].data[2] - pieChart.data.datasets[0].data[1] - pieChart.data.datasets[0].data[0];
+
+    pieChart.update();
+
+    setTimeout(updateChart, 500);
 }
